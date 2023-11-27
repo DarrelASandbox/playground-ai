35ded974f6f0a2e0938b8d796a695029534a4fb9
DarrelA <darrelaiscoding@gmail.com>
2023-11-27 12:20:22 +0800

    Adding a chain

--- chatgp-langchain/stephen-grider/main.py
+++ chatgp-langchain/stephen-grider/main.py
@@ -1,8 +1,17 @@
 from langchain.llms.openai import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 API_KEY = ""
 
 llm = OpenAI(openai_api_key=API_KEY)
 
-result = llm("Write a very short poem")
+code_prompt = PromptTemplate(
+    template="Write a short {language} function that will {task}",
+    input_variables=["language", "task"],
+)
+
+code_chain = LLMChain(llm=llm, prompt=code_prompt)
+
+result = code_chain({"language": "python", "task": "return a list of numbers"})
 print(result)
