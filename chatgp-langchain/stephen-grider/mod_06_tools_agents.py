23628cd2f7b4a4543774a8d3e2f68b7f12daffea
DarrelA <darrelaiscoding@gmail.com>
2023-12-03 22:14:18 +0800

    Added docstrings & comments

--- chatgp-langchain/stephen-grider/mod_06_tools_agents.py
+++ chatgp-langchain/stephen-grider/mod_06_tools_agents.py
@@ -1,3 +1,29 @@
+"""
+This script integrates various components from the langchain library to create an 
+AI agent capable of interacting with an SQLite database. 
+It demonstrates how to set up a chat-based agent that can understand and 
+respond to queries about the database, utilizing custom tools for 
+running SQL queries and describing table schemas.
+
+The script utilizes the OpenAIFunctionsAgent from langchain, 
+along with custom tools for database interaction. 
+The agent is configured with a specific prompt template and 
+is capable of executing SQL queries and describing database tables.
+
+Functions:
+- list_tables: Retrieves the list of table names from the SQLite database.
+- run_sqlite_query: Executes an SQL query against the SQLite database and returns the results.
+- describe_tables: Provides the schema of specified tables in the SQLite database.
+
+Classes:
+- RunQueryArgsSchema: Pydantic schema for the 'run_sqlite_query' function arguments.
+- DescribeTablesArgsSchema: Pydantic schema for the 'describe_tables' function arguments.
+
+Usage:
+The script is designed to be executed as a standalone module. 
+Upon running, it initializes an agent capable of processing natural language queries
+related to the database and executing corresponding SQL commands.
+"""
 from dotenv import load_dotenv
 from langchain.agents import AgentExecutor, OpenAIFunctionsAgent
 from langchain.chat_models import ChatOpenAI
@@ -13,8 +39,11 @@ load_dotenv()
 
 chat = ChatOpenAI()
 
-# [('users',), ('addresses',), ('products',), ('carts',), ('orders',), ('order_products',)]
+# Retrieve the list of tables in the SQLite database
 TABLES = list_tables()
+
+# Define the chat prompt template with a system message
+# and placeholders for user input and agent scratchpad
 prompt = ChatPromptTemplate(
     messages=[
         SystemMessage(
@@ -30,8 +59,14 @@ prompt = ChatPromptTemplate(
     ]
 )
 
+# Define tools for running SQL queries and describing table schemas
 tools = [run_query_tool, describe_tables_tool]
 
+# Create an OpenAIFunctionsAgent with the defined prompt and tools
 agent = OpenAIFunctionsAgent(llm=chat, prompt=prompt, tools=tools)
+
+# Create an AgentExecutor to execute the agent with verbose logging
 agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools)
+
+# Execute the agent to answer a query
 agent_executor("How many users have provided a shipping address?")
--- chatgp-langchain/stephen-grider/tools/sql.py
+++ chatgp-langchain/stephen-grider/tools/sql.py
@@ -1,21 +1,56 @@
+"""
+This module provides tools for interacting with an SQLite database using the langchain library. 
+It includes functions to list tables in the database, execute SQL queries,
+and describe the schemas of specific tables. 
+These tools are designed to be used with the langchain's OpenAIFunctionsAgent
+for natural language processing and database querying.
+
+Functions:
+- list_tables: Lists the names of all tables in the SQLite database.
+- run_sqlite_query: Executes a given SQL query on the SQLite database and returns the results.
+- describe_tables: Provides the schema for specified tables in the database.
+
+Classes:
+- RunQueryArgsSchema: Defines the schema for arguments to the 'run_sqlite_query' function.
+- DescribeTablesArgsSchema: Defines the schema for arguments to the 'describe_tables' function.
+
+Usage:
+These functions are intended to be used as tools within the langchain framework, particularly
+with OpenAIFunctionsAgent for processing natural language queries related to database operations.
+"""
 import sqlite3
 from typing import List
 
 from langchain.tools import Tool
 from pydantic.v1 import BaseModel
 
+# Connect to the SQLite database
 conn = sqlite3.connect("db.sqlite")
 
 
 def list_tables():
+    """
+    Retrieves a list of all table names in the SQLite database.
+
+    Returns:
+        str: A newline-separated string of table names.
+    """
     c = conn.cursor()
     c.execute("SELECT name FROM sqlite_master WHERE type='table';")
     rows = c.fetchall()
-    # Format the list of tuples for ChatGPT
     return "\n".join(row[0] for row in rows if row[0] is not None)
 
 
 def run_sqlite_query(query):
+    """
+    Executes a SQL query against the SQLite database.
+
+    Args:
+        query (str): The SQL query to be executed.
+
+    Returns:
+        list: The result of the SQL query execution, or an error message in case of failure.
+    """
     c = conn.cursor()
     try:
         c.execute(query)
@@ -28,6 +63,7 @@ class RunQueryArgsSchema(BaseModel):
     query: str
 
 
+# Define a tool for running SQL queries
 run_query_tool = Tool.from_function(
     name="run_sqlite_query",
     description="Run a sqlite query.",
@@ -37,6 +73,15 @@ run_query_tool = Tool.from_function(
 
 
 def describe_tables(table_names):
+    """
+    Describes the schema of the specified tables in the SQLite database.
+
+    Args:
+        table_names (List[str]): A list of table names whose schema is to be retrieved.
+
+    Returns:
+        str: A newline-separated string containing the schema of each specified table.
+    """
     c = conn.cursor()
     tables = ", ".join("'" + table + "'" for table in table_names)
     rows = c.execute(
@@ -49,6 +94,7 @@ class DescribeTablesArgsSchema(BaseModel):
     table_names: List[str]
 
 
+# Define a tool for describing table schemas
 describe_tables_tool = Tool.from_function(
     name="describe_tables",
     description="Given a list of table names, returns the schema of those tables",
