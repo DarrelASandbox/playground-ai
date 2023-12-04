48c590aa3e3c9f4899023a6efa5937fdf1e28986
DarrelA <darrelaiscoding@gmail.com>
2023-12-04 10:07:22 +0800

    `ConversationBufferMemory` with `AgentExecutor`

--- chatgp-langchain/stephen-grider/mod_06_tools_agents.py
+++ chatgp-langchain/stephen-grider/mod_06_tools_agents.py
@@ -1,14 +1,13 @@
 """
 This script integrates various components from the langchain library to create an 
-AI agent capable of interacting with an SQLite database. 
-It demonstrates how to set up a chat-based agent that can understand and 
-respond to queries about the database, utilizing custom tools for 
-running SQL queries and describing table schemas.
+AI agent capable of interacting with an SQLite database. It demonstrates how to set 
+up a chat-based agent that can understand and respond to queries about the database, 
+utilizing custom tools for running SQL queries and describing table schemas.
 
-The script utilizes the OpenAIFunctionsAgent from langchain, 
-along with custom tools for database interaction. 
-The agent is configured with a specific prompt template and 
-is capable of executing SQL queries and describing database tables.
+The script utilizes the OpenAIFunctionsAgent from langchain, along with custom tools 
+for database interaction. The agent is configured with a specific prompt template and 
+is enhanced with a conversation memory buffer, allowing it to maintain context over 
+multiple interactions. It is capable of executing SQL queries and describing database tables.
 
 Functions:
 - list_tables: Retrieves the list of table names from the SQLite database.
@@ -18,15 +17,18 @@ Functions:
 Classes:
 - RunQueryArgsSchema: Pydantic schema for the 'run_sqlite_query' function arguments.
 - DescribeTablesArgsSchema: Pydantic schema for the 'describe_tables' function arguments.
+- ConversationBufferMemory: A memory buffer that retains the conversation history for the agent.
 
 Usage:
-The script is designed to be executed as a standalone module. 
-Upon running, it initializes an agent capable of processing natural language queries
-related to the database and executing corresponding SQL commands.
+The script is designed to be executed as a standalone module. Upon running, it initializes 
+an agent capable of processing natural language queries related to the database and executing 
+corresponding SQL commands. The conversation memory buffer aids in maintaining context 
+across multiple queries.
 """
 from dotenv import load_dotenv
 from langchain.agents import AgentExecutor, OpenAIFunctionsAgent
 from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
 from langchain.prompts import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
@@ -36,6 +38,7 @@ from langchain.schema import SystemMessage
 from tools.report import write_report_tool
 from tools.sql import describe_tables_tool, list_tables, run_query_tool
 
+
 load_dotenv()
 
 chat = ChatOpenAI()
@@ -43,8 +46,7 @@ chat = ChatOpenAI()
 # Retrieve the list of tables in the SQLite database
 TABLES = list_tables()
 
-# Define the chat prompt template with a system message
-# and placeholders for user input and agent scratchpad
+# Define the chat prompt template with system message and placeholders
 prompt = ChatPromptTemplate(
     messages=[
         SystemMessage(
@@ -55,22 +57,24 @@ prompt = ChatPromptTemplate(
                 "or what columns exists. Instead, use the 'describe_tables' function"
             )
         ),
+        MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
 
-# Define tools for running SQL queries and describing table schemas
+# Initialize a conversation memory buffer to retain context over multiple interactions
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+# Define tools for running SQL queries, describing table schemas, and writing reports
 tools = [run_query_tool, describe_tables_tool, write_report_tool]
 
-# Create an OpenAIFunctionsAgent with the defined prompt and tools
+# Create an OpenAIFunctionsAgent with the defined prompt, tools, and conversation memory
 agent = OpenAIFunctionsAgent(llm=chat, prompt=prompt, tools=tools)
 
-# Create an AgentExecutor to execute the agent with verbose logging
-agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools)
+# Create an AgentExecutor to execute the agent with verbose logging and conversation memory
+agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools, memory=memory)
 
-# Execute the agent to answer a query
-# agent_executor("How many users have provided a shipping address?")
-agent_executor(
-    "Summarize the top 5 most popular products. Write the results to a report file."
-)
+# Execute the agent to answer queries and potentially write reports
+agent_executor("How many orders are there? Write the result to an html report.")
+agent_executor("Repeat the exact same process for users.")
