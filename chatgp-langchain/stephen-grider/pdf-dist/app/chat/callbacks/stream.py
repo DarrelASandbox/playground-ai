b4fcb16b24640e7b780078e5d8bec96da000a31b
DarrelA <darrelaiscoding@gmail.com>
2023-12-15 00:03:14 +0800

    Listen to events coming from models with streaming is true

--- chatgp-langchain/stephen-grider/pdf-dist/app/chat/callbacks/stream.py
+++ chatgp-langchain/stephen-grider/pdf-dist/app/chat/callbacks/stream.py
@@ -1,9 +1,18 @@
+from typing import Any, Dict, List, Optional
+from uuid import UUID
 from langchain.callbacks.base import BaseCallbackHandler
+from langchain.schema.messages import BaseMessage
 
 
 class StreamingHandler(BaseCallbackHandler):
     def __init__(self, queue):
         self.queue = queue
+
+    def on_chat_model_start(self, serialized, messages, run_id, **kwargs):
+        if serialized["kwargs"]["streaming"]:
+            print(
+                f"This is a streaming model! I should listen to events with run_id: {run_id}"
+            )
 
     def on_llm_new_token(self, token, **kwargs):
         self.queue.put(token)
