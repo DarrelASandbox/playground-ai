3a75205e9703c51cc70bb4f653e4671d81420974
DarrelA <darrelaiscoding@gmail.com>
2023-12-15 19:59:11 +0800

    Generalizing component picking

--- chatgp-langchain/stephen-grider/pdf-dist/app/chat/chat.py
+++ chatgp-langchain/stephen-grider/pdf-dist/app/chat/chat.py
@@ -3,33 +3,59 @@ import random
 from langchain.chat_models import ChatOpenAI
 
 from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
-from app.chat.llms.chatopenai import build_llm
-from app.chat.memories.sql_memory import build_memory
+from app.chat.llms import llm_map
+from app.chat.memories import memory_map
 from app.chat.models import ChatArgs
 from app.chat.vector_stores import retriever_map
 from app.web.api import get_conversation_components, set_conversation_components
 
 
-def build_chat(chat_args: ChatArgs):
+def select_component(component_type, component_map, chat_args):
     components = get_conversation_components(chat_args.conversation_id)
-    previous_retriever = components["retrieval"]
-    if previous_retriever:
-        build_retriever = retriever_map[previous_retriever]
-        retriever = build_retriever(chat_args)
-    else:
-        random_retriever_name = random.choice(list(retriever_map.keys()))
-        build_retriever = retriever_map[random_retriever_name]
-        retriever = build_retriever(chat_args)
-        set_conversation_components(
-            conversation_id=chat_args.conversation_id,
-            llm="",
-            memory="",
-            retriever=random_retriever_name,
-        )
+    previous_component = components[component_type]
+    if previous_component:
+        builder = component_map[previous_component]
+        return previous_component, builder(chat_args)
 
-    llm = build_llm(chat_args)
+    random_name = random.choice(list(component_map.keys()))
+    builder = component_map[random_name]
+    return random_name, builder(chat_args)
+
+
+def build_chat(chat_args: ChatArgs):
+    retriever_name, retriever = select_component(
+        "retriever",
+        retriever_map,
+        chat_args,
+    )
+    llm_name, llm = select_component(
+        "llm",
+        llm_map,
+        chat_args,
+    )
+    memory_name, memory = select_component(
+        "memory",
+        memory_map,
+        chat_args,
+    )
+
+    # For some reason, the second question is not using another retriever component
+    # Input: What country produces the most spice?
+    # Output: Running chain with: memory: sql_buffer_memory, llm: gpt-4, retriever: pinecone_1
+    # Input: and second?
+    # Output: Running chain with: memory: sql_buffer_memory, llm: gpt-4, retriever: pinecone_1
+    print(
+        f"Running chain with: memory: {memory_name}, llm: {llm_name}, retriever: {retriever_name}"
+    )
+
+    set_conversation_components(
+        chat_args.conversation_id,
+        llm=llm_name,
+        retriever=retriever_name,
+        memory=memory_name,
+    )
+
     condense_question_llm = ChatOpenAI(streaming=False)
-    memory = build_memory(chat_args)
 
     return StreamingConversationalRetrievalChain.from_llm(
         llm=llm,
