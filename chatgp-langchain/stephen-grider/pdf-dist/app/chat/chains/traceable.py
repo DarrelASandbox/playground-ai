da3c99e40df7ade80505885ed9da3541f2fadb83
DarrelA <darrelaiscoding@gmail.com>
2023-12-15 23:11:16 +0800

    Automatic trace creation

---
+++ chatgp-langchain/stephen-grider/pdf-dist/app/chat/chains/traceable.py
@@ -0,0 +1,22 @@
+from langfuse.model import CreateTrace
+
+from app.chat.tracing.langfuse import langfuse
+
+
+class TraceableChain:
+    def __call__(self, *args, **kwargs):
+        print("\nPrinting from app.chat.chains.traceable\n")
+        print(self.metadata)
+
+        trace = langfuse.trace(
+            CreateTrace(
+                id=self.metadata["conversation_id"],
+                metadata=self.metadata,
+            )
+        )
+
+        callbacks = kwargs.get("callbacks", [])
+        callbacks.append(trace.getNewHandler())
+        kwargs["callbacks"] = callbacks
+
+        return super().__call__(*args, **kwargs)
