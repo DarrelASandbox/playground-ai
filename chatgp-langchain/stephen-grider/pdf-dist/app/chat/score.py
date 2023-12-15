2afc4db85430b031c03bac2889d2157749396686
DarrelA <darrelaiscoding@gmail.com>
2023-12-15 20:58:29 +0800

    Selecting components by score

--- chatgp-langchain/stephen-grider/pdf-dist/app/chat/score.py
+++ chatgp-langchain/stephen-grider/pdf-dist/app/chat/score.py
@@ -1,4 +1,34 @@
+import random
+
 from app.chat.redis import client
+
+
+def random_component_by_score(component_type, component_map):
+    if component_type not in ["llm", "retriever", "memory"]:
+        raise ValueError("Invalid component_type")
+
+    values = client.hgetall(f"{component_type}_score_values")
+    counts = client.hgetall(f"{component_type}_score_counts")
+
+    # Get all the valid component names from the component map
+    names = component_map.keys()
+
+    avg_scores = {}
+
+    # Calculating the average score
+    for name in names:
+        score = int(values.get(name, 1))
+        count = int(counts.get(name, 1))
+        avg = score / count
+        avg_scores[name] = max(avg, 0.1)
+
+    sum_scores = sum(avg_scores.values())
+    random_val = random.uniform(0, sum_scores)
+    cumulative = 0
+    for name, score in avg_scores.items():
+        cumulative += score
+        if random_val <= cumulative:
+            return name
 
 
 def score_conversation(
